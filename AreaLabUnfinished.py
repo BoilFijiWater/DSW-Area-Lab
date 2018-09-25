a099ba1cc1f7cb86dde4e672dde899ef54143592
SiLordOfLight <jatnerd@gmail.com>
2018-09-25 11:12:39 -0700

    asfdadsg

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -1,8 +1,8 @@
-import math 
-import unittest
-
+import math #lets us use the math module 
+import unittest #unittest module helps is test small sections of code 
+ 
 def circleArea(radius): 
-    return 1
+    return radius * radius *math.pi    
 
 def rectArea(base, height):
     return 1
@@ -14,13 +14,17 @@ def triArea(base,height):
     return 1
     
 	
-class MyTest(unittest.TestCase):
+class MyTest(unittest.TestCase): #using TestCase class from unittest module
     def testCircleArea(self):
-    
+        self.assertEqual(circleArea(5),25*math.pi)
+        self.assertAlmostEqual(circleArea(10),100*math.pi)
     def testRectArea(self):
+    self.assertEqual(rectangle(50),250*math)
     
     def testTrapArea(self):
     
     def testTriArea(self):
+    
+    
     
     
